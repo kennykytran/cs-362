7efa7e87e1603b0e66bd6fdce94c21170dcbcdd4
Sam <huy.le09@yahoo.com>
2021-10-27 19:19:23 -0700

    Config end point for login and register

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
 const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(cors());
 
 app.get("/", (req, res) => {
     res.render("index", {});
-    
 });
 
-app.get("/", (req, res) => {
+app.get("/login", (req, res) => {
     res.render("login", {});
 });
 
-app.get("/", (req, res) => {
-    res.render("regis", {});
+app.get("/register", (req, res) => {
+    res.render("register", {});
 });
 
-app.get("/test", (req, res) => {
-    console.log("HIT TEST");
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(
+        `Example app listening at http://localhost:${process.env.PORT}`
+    );
 });
