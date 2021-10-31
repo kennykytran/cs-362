53203549d495f69089045137f20b6af3f8be788d
Sam <huy.le09@yahoo.com>
2021-10-30 22:26:27 -0700

    Added logic to create new user

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const db = require("./db");
+const bcrypt = require("bcrypt");
+
+class User {
+    static createNewUser = (newUser, done) => {
+        db.query(
+            "SELECT * FROM user WHERE username = ?",
+            [newUser.username],
+            async (err, result) => {
+                if (err) {
+                    console.log(err);
+                    done(err);
+                    return;
+                }
+
+                if (result.length > 0) {
+                    console.log("Username already existed");
+                    done("Username already existed");
+                    return;
+                }
+
+                const saltRound = 10;
+                const salt = await bcrypt.genSalt(saltRound);
+                const hashedPassword = await bcrypt.hash(
+                    newUser.password,
+                    salt
+                );
+
+                db.query(
+                    "INSERT INTO user (username, hash) VALUES (?, ?)",
+                    [newUser.username, hashedPassword],
+                    (err) => {
+                        if (err) {
+                            done(err);
+                            return;
+                        }
+                        console.log(`New username ${newUser.username} created`);
+                    }
+                );
+            }
+        );
+    };
+}
+
+module.exports = User;
