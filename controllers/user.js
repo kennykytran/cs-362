605dd5ab8597de48d940c129e832ac89b3e5acf3
Sam <huy.le09@yahoo.com>
2021-11-16 20:11:48 -0800

    User controller updated

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,33 @@
+const passport = require("passport");
+
 const User = require("../models/user");
 
-exports.displayForm = (req, res) => {
-    res.render("/register");
+exports.displayRegisterForm = (req, res) => {
+    res.render("register");
 };
 
-exports.registerUser = (req, res) => {
+exports.register = (req, res) => {
     User.createNewUser(req.body.user, (err) => {
         if (err) {
             res.redirect("/register");
             return;
         }
-        res.redirect("/login");
+        res.redirect("/");
     });
+};
+
+exports.displayLoginForm = (req, res) => {
+    res.render("login");
+};
+
+exports.login = () => {
+    return passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login-failure",
+    });
+};
+
+exports.logout = (req, res) => {
+    req.logout();
+    res.redirect("/login");
 };
