32004d16ee7f368d64df1887f168ee4f1a29b034
Sam <huy.le09@yahoo.com>
2021-11-17 22:23:59 -0800

    Restore to prior version, somebody overwrite by mistake

--- app.js
+++ app.js
@@ -1,22 +1,72 @@
-const express = require("express");
 require("dotenv").config();
 
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+
+const db = require("./models/db");
+const localStrategy = require("./utils/authStrategy").localStrategy;
+
+// ---------------------- GENERAL SETUP ----------------------
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
+// ---------------------- SESSION STORE SETUP ----------------------
+const sessionStore = new MySQLStore({}, db);
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET_KEY,
+        resave: false,
+        saveUninitialized: false,
+        store: sessionStore,
+        cookie: {
+            maxAge: 1000 * 60 * 60, // 1 hours cookie expiration date
+        },
+    })
+);
+
+// ---------------------- PASSPORT CONFIGURATION ----------------------
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(localStrategy);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    db.query("SELECT * FROM user WHERE id=?", [id], (err, result) => {
+        if (err) {
+            done(err);
+            return;
+        }
+        user = result[0];
+        done(err, user.id);
+    });
+});
+
+// ---------------------- ROUTES ----------------------
+const userRoutes = require("./routes/users");
+
+app.use(userRoutes);
+
+app.get("/secret", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.status(200).send("PRIVATE INFO");
+    } else {
+        res.status(400).send("You need to sign in");
+    }
+});
+
+// -----------------------------------------------------
 app.get("/", (req, res) => {
-    res.render("index", {});
-});
-
-app.get("/login", (req, res) => {
-    res.render("login", {});
-});
-
-app.get("/register", (req, res) => {
-    res.render("register", {});
+    res.render("index");
 });
 
 app.listen(port, () => {
