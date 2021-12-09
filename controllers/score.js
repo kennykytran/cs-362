d50ec7eda8c6a39d0db87a2599b65bc77987da29
Sam <huy.le09@yahoo.com>
2021-12-08 17:27:39 -0800

    Pass score to front end

--- controllers/score.js
+++ controllers/score.js
@@ -1,3 +1,15 @@
+const axios = require("axios");
+
 exports.showUserScore = (req, res) => {
-    res.render("score");
+    axios
+        .get(`sonnyleetan.xyz:8000/score/sonny`)
+        .then((response) => {
+            let userScore = response.data;
+            res.render("score", { userScore: userScore });
+        })
+        .catch((err) => {
+            res.render("score", {
+                userScore: "CANT RETRIEVE SCORE AT THE MOMENT",
+            });
+        });
 };
